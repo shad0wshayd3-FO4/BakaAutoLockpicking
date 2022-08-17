e53b7ed4a66a8723f96d6e4e3a120898a0e56524
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2022-08-17 14:00:00 -0600

    Initial commit

---
+++ src/c++/main.cpp
@@ -0,0 +1,59 @@
+namespace
+{
+	void InitializeLog()
+	{
+		auto path = logger::log_directory();
+		if (!path)
+		{
+			stl::report_and_fail("Failed to find standard logging directory"sv);
+		}
+
+		*path /= fmt::format(FMT_STRING("{:s}.log"), Version::PROJECT);
+		auto sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>(path->string(), true);
+
+		auto log = std::make_shared<spdlog::logger>("global log"s, std::move(sink));
+		auto lvl = *Settings::General::EnableDebugLogging
+					   ? spdlog::level::trace
+					   : spdlog::level::info;
+
+		log->set_level(lvl);
+		log->flush_on(lvl);
+
+		spdlog::set_default_logger(std::move(log));
+		spdlog::set_pattern("[%m/%d/%Y - %T] [%^%l%$] %v"s);
+
+		logger::info(FMT_STRING("{:s} v{:s}"), Version::PROJECT, Version::NAME);
+	}
+}
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Query(const F4SE::QueryInterface* a_F4SE, F4SE::PluginInfo* a_info)
+{
+	a_info->infoVersion = F4SE::PluginInfo::kVersion;
+	a_info->name = Version::PROJECT.data();
+	a_info->version = Version::MAJOR;
+
+	const auto rtv = a_F4SE->RuntimeVersion();
+	if (rtv < F4SE::RUNTIME_LATEST)
+	{
+		stl::report_and_fail(
+			fmt::format(
+				FMT_STRING("{:s} does not support runtime v{:s}."),
+				Version::PROJECT,
+				rtv.string()));
+	}
+
+	return true;
+}
+
+extern "C" DLLEXPORT bool F4SEAPI F4SEPlugin_Load(const F4SE::LoadInterface* a_F4SE)
+{
+	Settings::Load();
+	InitializeLog();
+
+	logger::info(FMT_STRING("{:s} loaded."), Version::PROJECT);
+	logger::debug("Debug logging enabled.");
+
+	F4SE::Init(a_F4SE);
+
+	return true;
+}
